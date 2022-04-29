278a55e1a4823a17745c860311ac92b4442d2060
blairMe <felixanachoni@gmail.com>
2022-04-29 17:56:47 +0300

    Fav Dish

--- app/src/main/java/bfa/blair/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/bfa/blair/favdish/view/fragments/AllDishesFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import bfa.blair.favdish.databinding.FragmentHomeBinding
+import bfa.blair.favdish.databinding.FragmentAllDishesBinding
 import bfa.blair.favdish.viewmodel.HomeViewModel
 
-class HomeFragment : Fragment() {
+class AllDishesFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAllDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
