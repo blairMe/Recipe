0109b67109dd89b559ef20870c0a9d1db7876f91
blairMe <felixanachoni@gmail.com>
2022-08-24 09:16:02 +0300

    Fav Dish

--- app/src/main/java/bfa/blair/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/bfa/blair/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,38 @@ package bfa.blair.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import bfa.blair.favdish.R
+import bfa.blair.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding : ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when(v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked on the imageview", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
     }
 }
