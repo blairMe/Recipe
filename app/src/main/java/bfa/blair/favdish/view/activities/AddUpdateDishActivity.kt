b75af08e0c86049a88cc6c6404704ca6440db758
blairMe <felixanachoni@gmail.com>
2022-09-05 22:21:37 +0300

    Fav Dish

--- app/src/main/java/bfa/blair/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/bfa/blair/favdish/view/activities/AddUpdateDishActivity.kt
@@ -21,6 +21,7 @@ import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -127,14 +128,22 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             if(requestCode == CAMERA) {
                 data?.extras?.let {
                     val thumbnail : Bitmap = data.extras!!.get("data") as Bitmap
-                    mBinding.ivDishImage.setImageBitmap(thumbnail)
+                    //mBinding.ivDishImage.setImageBitmap(thumbnail)
+                    Glide.with(this)
+                        .load(thumbnail)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
 
                     mBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
               } }
             if(requestCode == GALLERY) {
                 data?.let {
                     val selectedPhotoUri = data.data
-                    mBinding.ivDishImage.setImageURI(selectedPhotoUri)
+                    //mBinding.ivDishImage.setImageURI(selectedPhotoUri)
+                    Glide.with(this)
+                        .load(selectedPhotoUri)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
 
                     mBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
                 }
