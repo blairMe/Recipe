257381939cf48a5667321eef22fdc8e3e339d29f
blairMe <felixanachoni@gmail.com>
2022-09-06 23:09:08 +0300

    Fav Dish

--- app/src/main/java/bfa/blair/favdish/utils/Constants.kt
+++ app/src/main/java/bfa/blair/favdish/utils/Constants.kt
@@ -1,14 +1,60 @@
 package bfa.blair.favdish.utils
 
 object Constants {
-    const val DISH_TYPE : String = "Dish type"
-    const val DISH_CATEGORY : String = "Dish Category"
-    const val DISH_COOKING_TIME : String = "Cooking Time"
+    const val DISH_TYPE : String = "DishType"
+    const val DISH_CATEGORY : String = "DishCategory"
+    const val DISH_COOKING_TIME : String = "CookingTime"
 
     fun dishTypes() : ArrayList<String> {
-        var list = ArrayList<String>()
+        val list = ArrayList<String>()
 
         list.add("Breakfast")
+        list.add("Lunch")
+        list.add("Snacks")
+        list.add("Dinner")
+        list.add("Salad")
+        list.add("Side Dish")
+        list.add("Desserts")
+        list.add("Others")
+
+        return list
+    }
+
+    fun dishCategories() : ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("Pizza")
+        list.add("BBQ")
+        list.add("Bakery")
+        list.add("Burger")
+        list.add("Cafe")
+        list.add("Chicken")
+        list.add("Dessert")
+        list.add("Drinks")
+        list.add("Hot Dogs")
+        list.add("Juices")
+        list.add("Sandwich")
+        list.add("Tea/Coffee")
+        list.add("Wraps")
+        list.add("Other")
+
+        return list
+    }
+
+    fun dishCookTime() : ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("10")
+        list.add("15")
+        list.add("20")
+        list.add("30")
+        list.add("45")
+        list.add("50")
+        list.add("60")
+        list.add("90")
+        list.add("120")
+        list.add("150")
+        list.add("180")
 
         return list
     }
