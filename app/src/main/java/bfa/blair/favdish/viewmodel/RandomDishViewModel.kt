fec810b6fd63c14e185042ef8f88d6289b3d7663
blairMe <felixanachoni@gmail.com>
2022-09-30 15:01:49 +0300

    Fav Dish

--- app/src/main/java/bfa/blair/favdish/viewmodel/RandomDishViewModel.kt
+++ app/src/main/java/bfa/blair/favdish/viewmodel/RandomDishViewModel.kt
@@ -3,11 +3,44 @@ package bfa.blair.favdish.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import bfa.blair.favdish.model.entities.RandomDish
+import bfa.blair.favdish.model.network.RandomDishAPIService
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class RandomDishViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private val randomRecipeApiService = RandomDishAPIService()
+
+    private val compositeDisposable = CompositeDisposable()
+
+    val loadRandomDish = MutableLiveData<Boolean>()
+    val randomDishRespose = MutableLiveData<RandomDish.Recipes>()
+    val randomDishLoadingError = MutableLiveData<Boolean>()
+
+    fun getRandomRecipeFromAPI() {
+        loadRandomDish.value = true
+        compositeDisposable.add(
+            randomRecipeApiService.getRandomDish()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<RandomDish.Recipes>() {
+                    override fun onSuccess(value: RandomDish.Recipes) {
+                        loadRandomDish.value = false
+                        randomDishRespose.value = value
+                        randomDishLoadingError.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        loadRandomDish.value = true
+                        randomDishLoadingError.value = true
+                        e!!.printStackTrace()
+                    }
+
+                })
+        )
+
     }
-    val text: LiveData<String> = _text
 }
